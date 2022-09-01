5e5d31cff5b0e9499ff785eb3455943ba4bac3b9
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-09-01 12:57:28 -0400

    schema work

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT  PRIMARY KEY,
+    VARCHAR (40)
+);
+
+CREATE TABLE roles(
+    department_id INTEGER,
+    title VARCHAR(40),
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    salary DECIMAL,
+    FOREIGN KEY (department_id)
+    REFERENCES  department(id)
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER.
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role_id,
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
+)
