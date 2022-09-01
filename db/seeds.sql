241beb156c148bff408d423edb59225a29613daa
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-09-01 10:56:00 -0400

    starting sql
    aded npm sql, json, db files

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO department (name_of_department)
+
+VALUE
+("Sales")
+("Security")
+("Logistics")
+("Inventory")
+("Legal")
+("Accounting")
+("Human Resources")
+("Customer Service")
+
+  INSERT INTO  roles (title, salary, department_id)
+  VALUE
+ ("Sales Director",95000,1)
+ ("Head of Security",80000,1)
+ ("Supply Chain Chief Officer",120000,2)
+ ("Lawyer",14000,2)
+ ("Chief Finance Officer",170000,2)
+ ("Human Resources Director",90000,2)
+ ("Customer Service Manager",75000,3)
+("Warehouse Manager",75000,3)
