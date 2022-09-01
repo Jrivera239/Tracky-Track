568cc1c23eca99e23c1342c294f79420b624334c
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-09-01 11:12:41 -0400

    seed sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO department (name_of_department)
-
 VALUE
+
 ("Sales")
 ("Security")
 ("Logistics")
@@ -12,6 +12,7 @@ VALUE
 
   INSERT INTO  roles (title, salary, department_id)
   VALUE
+
  ("Sales Director",95000,1)
  ("Head of Security",80000,1)
  ("Supply Chain Chief Officer",120000,2)
@@ -20,3 +21,18 @@ VALUE
  ("Human Resources Director",90000,2)
  ("Customer Service Manager",75000,3)
 ("Warehouse Manager",75000,3)
+
+INSERT INTO Employee(first_name, last_name, manager_id, role_id)
+VALUE
+
+('Melissa','Castillo', '1','null'),
+('Joel','Rivera', '2','null'),
+('Demont','Rein', '3','1'),
+('Edward','Gonzales', '4','2'),
+('Ryan','Ogden', '5','1'),
+('Olivia','Khan', '6','2'),
+('Robert','Teyira', '7','3'),
+('Carl','Andersen', '8','3'),
+
+
+
