c92382855955cf8d79f49b396806476f42fab0d1
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-09-08 15:31:32 -0400

    index and connection, seed
    addiing roles for sql selection, index, schema and connect files for ports.

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,11 @@
 const mysql2 = require('mysql2');
-
-// Connect to database
+// Connect to DB
 const db = mysql2.createConnection ({
     host: 'localhost',
+    port:3306,
     user: 'root',
     password: 'qwery1235',
     database: 'employee'
-    },
-    console.log('Welcome!')
-);
-
+});
+console.log('Welcome!')
 module.exports = db;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,7 @@
 INSERT INTO department (name_of_department)
 VALUE
 
+
 ("Sales")
 ("Security")
 ("Logistics")
@@ -10,8 +11,10 @@ VALUE
 ("Human Resources")
 ("Customer Service")
 
+
   INSERT INTO  roles (title, salaries, department_id)
   VALUE
+
 
  ("Sales Director",95000,1)
  ("Head of Security",80000,1)
@@ -21,6 +24,7 @@ VALUE
  ("Human Resources Director",90000,2)
  ("Customer Service Manager",75000,3)
 ("Warehouse Manager",75000,3)
+
 
 INSERT INTO Employee(first_name, last_name, manager_id, role_id)
 VALUE
@@ -34,6 +38,24 @@ VALUE
 ('Robert','Teyira', '7','3'),
 ('Carl','Andersen', '8','3'),
 
-SELECT * FROM role;
-SELECT * FROM department;
-SELECT * FROM employee;
+ -- select/from --
+SELECT 
+
+    deparment.department_name AS Department
+    roles.salaries AS Salaries,
+    roles.id AS Id,
+    employees.first_name AS First_Name,
+    employees.last_name AS Last_Name,
+    roles.title AS Title,
+    employees.id AS Id,
+    department.department_name AS Department,
+    CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
+    FROM employees 
+    LEFT JOIN roles on employees.role_id = roles.id 
+    LEFT JOIN department on roles.department_id = department.id 
+    LEFT JOIN employees manager on manager.id = employees.manager_id 
+    ORDER BY department_name ASC;
+
+    SELECT * FROM role;
+    SELECT * FROM department;
+    SELECT * FROM employee; 
