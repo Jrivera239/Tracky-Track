737f2a43228496e9cb0470851c6a089857b06eff
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-10-04 19:15:58 -0400

    updating seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,28 +2,28 @@ INSERT INTO department (name_of_department)
 VALUE
 
 
-("Sales")
-("Security")
-("Logistics")
-("Inventory")
-("Legal")
-("Accounting")
-("Human Resources")
-("Customer Service")
+("Sales"),
+("Security"),
+("Logistics"),
+("Inventory"),
+("Legal"),
+("Accounting"),
+("Human Resources"),
+("Customer Service");
 
 
   INSERT INTO  roles (title, salaries, department_id)
   VALUE
 
 
- ("Sales Director",95000,1)
- ("Head of Security",80000,1)
- ("Supply Chain Chief Officer",120000,2)
- ("Lawyer",14000,2)
- ("Chief Finance Officer",170000,2)
- ("Human Resources Director",90000,2)
- ("Customer Service Manager",75000,3)
-("Warehouse Manager",75000,3)
+ ("Sales Director",95000,1),
+ ("Head of Security",80000,1),
+ ("Supply Chain Chief Officer",120000,2),
+ ("Lawyer",14000,2),
+ ("Chief Finance Officer",170000,2),
+ ("Human Resources Director",90000,2),
+ ("Customer Service Manager",75000,3),
+("Warehouse Manager",75000,3);
 
 
 INSERT INTO Employee(first_name, last_name, manager_id, role_id)
